472cf9b59024b3b3175c1411a7aef45efb504014
Cr-Sa <liluo952@gmail.com>
2021-09-21 04:43:18 -0300

    Se agregan las entidades

---
+++ MicroservicioCliente/PS.Template.Domain/Entities/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PS.Template.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public int UsuarioId { get; set; }
+        public int PlanId { get; set; }
+        public int PartidoId { get; set; }
+    }
+}
