413a68e50d0e3e3eeb97aca209acd707a3ff6cd2
Cr-Sa <liluo952@gmail.com>
2021-09-21 04:12:59 -0300

    Se agrega el microservicioCliente

---
+++ MicroservicioCliente/PS.Template.Application/Services/IDocenteService.cs
@@ -0,0 +1,10 @@
+﻿using PS.Template.Domain.DTOs;
+using PS.Template.Domain.Entities;
+
+namespace PS.Template.Application.Services
+{
+    public interface IDocenteService
+    {
+        Docente CreateDocente(DocenteDTO docente);
+    }
+}
