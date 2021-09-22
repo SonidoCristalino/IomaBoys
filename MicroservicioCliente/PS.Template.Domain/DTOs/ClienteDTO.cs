85d709fa6255db83263e7d6de62af9b37ddd51d6
bazangeronimo <bazangeronimo@gmail.com>
2021-09-22 19:30:55 -0300

    Se agregaron las DTO

---
+++ MicroservicioCliente/PS.Template.Domain/DTOs/ClienteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Template.Domain.DTOs
+{
+    public class ClienteDTO
+    {
+        public int UsuarioId { get; set; }
+        public int PlanId { get; set; }
+        public int PartidoId { get; set; }
+
+    }
+}
